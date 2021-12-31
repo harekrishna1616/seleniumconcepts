fb40f3c7a18cd72fa8c0555562219cdb58fd6620
Radhe Krishna <you@example.com>
2021-12-31 15:04:45 +0530

    chrome and edge exe added

--- src/test/java/local/qa/seleniumconceptstest/GeneralBasicTest.java
+++ src/test/java/local/qa/seleniumconceptstest/GeneralBasicTest.java
@@ -1,8 +1,31 @@
 package local.qa.seleniumconceptstest;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
 public class GeneralBasicTest {
 
 	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "D:/My Projects - Eclipse/Drivers/chromedriver_win32/chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.get("https://demoqa.com/automation-practice-form");
+		driver.quit();
+		
+		System.setProperty("webdriver.edge.driver", "D:\\My Projects - Eclipse\\Drivers\\edgedriver_win64\\msedgedriver.exe");
+		driver=new EdgeDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.get("https://demoqa.com/automation-practice-form");
+		driver.quit();
+		
+		
+		
+		
+		
 		
 	}
 
